747115617f670f630fc1e8a42349bc587a6785b8
Usuario <Usuario@CPD03-M09>
2024-03-18 15:00:51 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppCalc/MainPage.xaml.cs
@@ -0,0 +1,100 @@
+﻿namespace MauiAppCalc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+        }
+
+        private void maismenos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multiplica_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void porcento_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dividir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num7_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num8_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num9_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multiplica_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num4_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num5_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num6_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void porcento_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num2_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num2_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num2_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num3_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void subtrair_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
